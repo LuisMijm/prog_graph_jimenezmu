1c5b69a011ac64ea80e06c3de93d215c1c1dea5d
Luis Miguel Jimenez Muñoz <jimenezmu@esat-alumni.com>
2024-05-09 15:41:28 +0100

    feat: GPU manager camera and program

--- T4_GPU_Manager_Workspace/T4_GPU_Manager_Workspace/include/dev/custom_gpu_manager.h
+++ T4_GPU_Manager_Workspace/T4_GPU_Manager_Workspace/include/dev/custom_gpu_manager.h
@@ -44,56 +44,55 @@ class CustomGPUManager : public EDK3::dev::GPUManager {
 
    /*
   virtual void newFramebuffer(EDK3::ref_ptr<Framebuffer>* output) override;
+   */
   virtual void clearFrameBuffer(const float rgba[4],
                                 const unsigned int FrameBufferOrMask =
                                 kFrameBufferColor | kFrameBufferDepth) override;
-   */
 
-   /*
+   
   virtual void enableDepthTest(const CompareFunc f) override;
   virtual void disableDepthTest() override;
-   */
+   
 
-   /*
+   
   virtual void enableBlend(BlendParam source, 
                            BlendParam destination, 
                            BlendOp operation, 
                            const float ConstantColor[4]) override;
   void disableBlend();
-   */
+   
 
    /*
   virtual void enableCullFaces(const FaceType f) override;
   virtual void disableCullFaces() override;
    */
 
-   /*
+   
   virtual void enableScissor(int x, int y,
                              unsigned int width,
                              unsigned int height) override;
   virtual void disableScissor() override;
-   */
+   
 
-   /*
+   
   virtual void enableVertexAttribute(const EDK3::dev::Buffer *buffer,
                                      const unsigned int attribute_index,
                                      const EDK3::Type type,
                                      const bool normalized = false,
                                      const unsigned int offset = 0,
                                      const unsigned int stride = 0) override;
-   */
+   
 
-   /*
+   
   virtual void disableVertexAttribute(const unsigned int attrib_index) override;
-   */
+   
 
-   /*
+   
   virtual void drawElements(const DrawMode mode,
                             unsigned int count,
                             const EDK3::dev::Buffer *buffer,
                             const EDK3::Type element_type = EDK3::Type::T_USHORT,
                             const unsigned int offset = 0) const override;
-   */
 
 }; //CustomGPUManager
 
--- T4_GPU_Manager_Workspace/T4_GPU_Manager_Workspace/src/dev/custom_gpu_texture.cc
+++ T4_GPU_Manager_Workspace/T4_GPU_Manager_Workspace/src/dev/custom_gpu_texture.cc
@@ -15,6 +15,85 @@ namespace EDK3 {
       return *this;
     }
 
+    GLenum GetOpenGLTextureType(EDK3::Texture::Type t) {
+        GLenum type = GL_TEXTURE_2D;
+        switch (t) {
+        case Texture::Type::T_Invalid:
+            //printf("Invalid texture type.\n");
+            //exit(EXIT_FAILURE);
+            break;
+        case Texture::Type::T_1D:
+            type = GL_TEXTURE_1D;
+            break;
+        case Texture::Type::T_3D:
+            type = GL_TEXTURE_3D;
+            break;
+        case Texture::Type::T_CUBEMAP:
+            type = GL_TEXTURE_CUBE_MAP;
+            break;
+        }
+        return type;
+    }
+
+    GLenum GetOpenGLDataPixelType(EDK3::Type t) {
+        GLenum type = GL_UNSIGNED_BYTE;
+        switch (t)
+        {
+        case EDK3::Type::T_UBYTE:
+            type = GL_UNSIGNED_BYTE;
+            break;
+        case EDK3::Type::T_SHORT:
+            type = GL_SHORT;
+            break;
+        case EDK3::Type::T_USHORT:
+            type = GL_UNSIGNED_SHORT;
+            break;
+        case EDK3::Type::T_INT:
+            type = GL_INT;
+            break;
+        case EDK3::Type::T_UINT:
+            type = GL_UNSIGNED_INT;
+            break;
+        case EDK3::Type::T_FLOAT:
+            type = GL_FLOAT;
+            break;
+        }
+        return type;
+    }
+
+    GLenum GetOpenGLFormat(EDK3::Texture::Format t) {
+        GLint format = GL_RED;
+        switch (t) {
+        case EDK3::Texture::Format::F_None:
+            format = GL_NONE;
+            break;
+        case EDK3::Texture::Format::F_R:
+            format = GL_RED;
+            break;
+        case EDK3::Texture::Format::F_RG:
+            format = GL_RG;
+            break;
+        case EDK3::Texture::Format::F_RGB:
+            format = GL_RGB;
+            break;
+        case EDK3::Texture::Format::F_BGR:
+            format = GL_BGR;
+            break;
+        case EDK3::Texture::Format::F_BGRA:
+            format = GL_BGRA;
+            break;
+        case EDK3::Texture::Format::F_RGBA:
+            format = GL_RGBA;
+            break;
+        case EDK3::Texture::Format::F_DEPTH:
+            format = GL_DEPTH_COMPONENT;
+            break;
+        case EDK3::Texture::Format::F_DEPTH32:
+            format = GL_DEPTH_COMPONENT32;
+            break;
+        }
+        return format;
+    }
 
     void CustomGPUTexture::init(Type t, Format internal_format, unsigned int width,
       unsigned int height, unsigned int depth) {
@@ -27,114 +106,154 @@ namespace EDK3 {
       const void* data,
       unsigned int mipmap_LOD) {
 
-      //glGenTextures
-      //bind
-      //glTexImage
-      //glPixelStorei
-      //glPixelstorei
+      glGenTextures(1, &internal_id_);
+      GLenum texture_type = GetOpenGLTextureType(type());
+      GLenum pixel_type = GetOpenGLDataPixelType(t);
+      GLint internal_format = GetOpenGLFormat(format());
+      GLint external_format = GetOpenGLFormat(f);
+      glBindTexture(texture_type, internal_id_);
+
+      switch (texture_type){
+      case GL_TEXTURE_1D:
+          glTexImage1D(texture_type, mipmap_LOD, 
+                       internal_format, width(), 0, 
+                       external_format, pixel_type, data);
+          break;
+
+      case GL_TEXTURE_2D:
+          glTexImage2D(texture_type, mipmap_LOD,
+              internal_format, width(), height(), 0,
+              external_format, pixel_type, data);
+          break;
+      case GL_TEXTURE_3D:
+          glTexImage3D(texture_type, mipmap_LOD,
+              internal_format, width(), height(),
+              depth(), 0,
+              external_format, pixel_type, data);
+          break;
+      }
+      glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+      glPixelStorei(GL_UNPACK_LSB_FIRST, 1);
+      generateMipmaps();
+      set_wrap(EDK3::Texture::Wrap::W_REPEAT);
+      set_min_filter(EDK3::Texture::Filter::F_LINEAR);
+      set_mag_filter(EDK3::Texture::Filter::F_LINEAR);
+
     }
 
     void CustomGPUTexture::bind(unsigned int textUnit) const {
-
+        GLenum texture_type = GetOpenGLTextureType(type());
+        glActiveTexture(GL_TEXTURE0 + textUnit);
+        glBindTexture(texture_type, internal_id_);
     }
 
     unsigned int CustomGPUTexture::internal_id() const {
       return internal_id_;
     }
 
+    GLenum GetOpenGLFilterType(EDK3::Texture::Filter f) {
+        GLenum filter_type = GL_NEAREST;
+
+        switch (f) {
+        case EDK3::Texture::F_NEAREST:
+            filter_type = GL_NEAREST;
+            break;
+
+        case EDK3::Texture::F_LINEAR:
+            filter_type = GL_LINEAR;
+            break;
+
+        case EDK3::Texture::F_NEAREST_MIPMAP_NEAREST:
+            filter_type = GL_NEAREST_MIPMAP_NEAREST;
+            break;
+
+        case EDK3::Texture::F_LINEAR_MIPMAP_NEAREST:
+            filter_type = GL_LINEAR_MIPMAP_NEAREST;
+            break;
+
+        case EDK3::Texture::F_NEAREST_MIPMAP_LINEAR:
+            filter_type = GL_NEAREST_MIPMAP_LINEAR;
+            break;
+
+        case EDK3::Texture::F_LINEAR_MIPMAP_LINEAR:
+            filter_type = GL_LINEAR_MIPMAP_NEAREST;
+            break;
+        }
+        return filter_type;
+    }
+
+    GLenum GetOpenGLWrappingType(EDK3::Texture::Wrap c) {
+        GLenum wrapping_type = GL_REPEAT;
+        switch (c){
+
+        case Texture::Wrap::W_REPEAT:
+            wrapping_type = GL_REPEAT;
+            break;
+
+        case EDK3::Texture::W_MIRRORED_REPEAT:
+            wrapping_type = GL_MIRRORED_REPEAT;
+            break;
+
+        case EDK3::Texture::W_CLAMP_TO_EDGE:
+            wrapping_type = GL_CLAMP_TO_EDGE;
+            break;
+        }
+        return wrapping_type;
+    }
+
     void CustomGPUTexture::set_min_filter(Filter f) {
       Texture::set_min_filter(f);
-      switch (f)
-      {
-      case EDK3::Texture::F_NEAREST:
-        //glTexParameteri();
-        break;
-      case EDK3::Texture::F_LINEAR:
-        //glTexParameteri();
+      GLenum texture_type = GetOpenGLTextureType(type());
+      GLenum filter_type = GetOpenGLFilterType(f);
 
-        break;
-      case EDK3::Texture::F_NEAREST_MIPMAP_NEAREST:
-        //glTexParameteri();
-
-        break;
-      case EDK3::Texture::F_LINEAR_MIPMAP_NEAREST:
-        //glTexParameteri();
-
-        break;
-      case EDK3::Texture::F_NEAREST_MIPMAP_LINEAR:
-        //glTexParameteri();
-
-        break;
-      case EDK3::Texture::F_LINEAR_MIPMAP_LINEAR:
-        //glTexParameteri();
-
-        break;
-      }
+      glBindTexture(texture_type, internal_id_);
+      glTexParameteri(texture_type, GL_TEXTURE_MIN_FILTER, filter_type);
     }
+
     void CustomGPUTexture::set_mag_filter(Filter f) {
       Texture::set_mag_filter(f);
-      switch (f)
-      {
-      case EDK3::Texture::F_NEAREST:
-        break;
-      case EDK3::Texture::F_LINEAR:
-        break;
-      }
+      GLenum texture_type = GetOpenGLTextureType(type());
+      GLenum filter_type = GetOpenGLFilterType(f);
 
+      glBindTexture(texture_type, internal_id_);
+      glTexParameteri(texture_type, GL_TEXTURE_MAG_FILTER, filter_type);
     }
+
     void CustomGPUTexture::set_wrap_s(Wrap c) {
       Texture::set_wrap_s(c);
-      switch (c)
-      {
-      case EDK3::Texture::W_REPEAT:
-        glTextureParameteri();
+      GLenum texture_type = GetOpenGLTextureType(type());
 
-        break;
-      case EDK3::Texture::W_MIRRORED_REPEAT:
-        glTextureParameteri();
+      GLenum wrapping_type = GetOpenGLWrappingType(c);
 
-        break;
-      case EDK3::Texture::W_CLAMP_TO_EDGE:
-        glTextureParameteri();
-
-        break;
-      }
+      glBindTexture(texture_type, internal_id_);
+      glTexParameteri(texture_type, GL_TEXTURE_WRAP_S, wrapping_type);
     }
     void CustomGPUTexture::set_wrap_t(Wrap c) {
       Texture::set_wrap_t(c);
-      switch (c)
-      {
-      case EDK3::Texture::W_REPEAT:
-        glTextureParameteri();
+      GLenum texture_type = GetOpenGLTextureType(type());
 
-        break;
-      case EDK3::Texture::W_MIRRORED_REPEAT:
-        glTextureParameteri();
+      GLenum wrapping_type = GetOpenGLWrappingType(c);
 
-        break;
-      case EDK3::Texture::W_CLAMP_TO_EDGE:
-        glTextureParameteri();
-
-        break;
+      glBindTexture(texture_type, internal_id_);
+      glTexParameteri(texture_type, GL_TEXTURE_WRAP_T, wrapping_type);
     }
+
     void CustomGPUTexture::set_wrap_r(Wrap c) {
       Texture::set_wrap_r(c);
-      switch (c)
-      {
-      case EDK3::Texture::W_REPEAT:
-        glTextureParameteri();
+      GLenum texture_type = GetOpenGLTextureType(type());
 
-        break;
-      case EDK3::Texture::W_MIRRORED_REPEAT:
-        glTextureParameteri();
+      GLenum wrapping_type = GetOpenGLWrappingType(c);
 
-        break;
-      case EDK3::Texture::W_CLAMP_TO_EDGE:
-        glTextureParameteri();
-
-        break;
+      glBindTexture(texture_type, internal_id_);
+      glTexParameteri(texture_type, GL_TEXTURE_WRAP_R, wrapping_type);
     }
+
     void CustomGPUTexture::generateMipmaps() const {
-      //glGenerateMipmap()
+        GLenum texture_type = GetOpenGLTextureType(Type());
+        glGenerateMipmap(texture_type);
+    }
+
+    CustomGPUTexture::~CustomGPUTexture()
+    {
     }
 }
